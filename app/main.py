237e3b103451a5f8dee3f01268df14879ebba956
Miguel Diaz <migueldi.tech@gmail.com>
2023-07-22 09:07:36 -0300

    Adicion de requirements.txt

--- app/main.py
+++ app/main.py
@@ -5,8 +5,14 @@ import charts
 
 def run():
   data = read_csv.read_csv('data.csv')
-  country = input('Type Country => ')
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
+  countries = list(map(lambda x: x['Country/Territory'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
+
+  country = input('Type Country => ')
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
