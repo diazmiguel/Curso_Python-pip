5702bd9c728d523768ecc20506b4a1064e1ba5a7
Miguel Diaz <migueldi.tech@gmail.com>
2023-07-24 13:24:15 -0300

    Agregacion de web fremework fireAPI

--- WebServer/main.py
+++ WebServer/main.py
@@ -1,4 +1,30 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app= FastAPI()
+
+@app.get('/')#sintaxis, decorador. Cual es la ruta x ingresar x la web
+def getList(): 
+    return [1,2,3,] #devuelve una lista
+
+@app.get('/contact') #sintaxis, decorador. Cual es la ruta x ingresar x la web
+def getList():
+    return { 'name': 'Platzi' }
+
+@app.get("/pagina", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Un titulo emocionante</title>
+        </head>
+        <body>
+            <h1>Un cuerpo genial!</h1>
+            <h2>Soy una figura ❤ </h2>
+        </body>
+    </html>
+    """
 
 def run():
     store.getCategories()
--- app/main.py
+++ app/main.py
@@ -1,25 +1,36 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
+  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  countries = list(map(lambda x: x['Country/Territory'], df))
+  percentages = list(map(lambda x: x['World Population Percentage'], df))
 
-  countries = list(map(lambda x: x['Country/Territory'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+  
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  
   charts.generate_pie_chart(countries, percentages)
 
-
+  #recordar que el manejo de pandas es diferente
+  data= read_csv.read_csv('data.csv')
   country = input('Type Country => ')
+  print(country)
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country/Territory'],labels, values)
-    charts.generate_pie_chart(labels, values)
+    #charts.generate_pie_chart(labels, values)
 
 
 if __name__ == '__main__':
